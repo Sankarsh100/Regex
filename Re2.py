fee4dad8a1c74d22171557ec168ef6a5da649d03
Sankarsh Sanap <sankarshgsanap@gmail.com>
2024-09-09 16:22:45 -0400

    commit

--- Re2.py
+++ Re2.py
@@ -19,3 +19,16 @@ for name in names:
     if match:
         print(name)
 
+#TEST if string starts with https or http
+
+Links = ['https://www.geeksforgeeks.org/python-regex-cheat-sheet/#basic',
+         'https://www.vdot.virginia.gov/',
+         'htts://github.com/Sankarsh100/Regex/pulls',
+         ]
+
+regex1 = 'https?'
+for link in Links:
+    if re.match(regex1,link):
+        print(link)
+
+
